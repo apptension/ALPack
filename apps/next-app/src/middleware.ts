7e3432dfc5dfbb22e855b477bf0a45633d6ea433
Marek Miklaszewski <mmiklaszewski@apptension.com>
2023-04-21 10:21:15 +0200

    [RAB-2] Create middleware for supabase auth

---
+++ apps/next-app/src/middleware.ts
@@ -0,0 +1,25 @@
+import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareSupabaseClient({ req, res });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session) {
+    // Authentication successful, forward request to protected route.
+    return res;
+  }
+
+  // Auth condition not met, redirect to home page.
+  const redirectUrl = req.nextUrl.clone();
+  redirectUrl.pathname = '/auth/login';
+  return NextResponse.redirect(redirectUrl);
+}
+
+export const config = {
+  matcher: '/dashboard',
+};
