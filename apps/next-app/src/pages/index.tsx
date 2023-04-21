850e0f7c00858416ff961cbc0b6f962a84c11001
Marek Miklaszewski <mmiklaszewski@apptension.com>
2023-04-21 16:46:07 +0200

    [RAB-3] Add links in homepage

--- apps/next-app/src/pages/index.tsx
+++ apps/next-app/src/pages/index.tsx
@@ -1,7 +1,24 @@
+import { ROUTES } from 'constants/ROUTES';
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl">Hello World!</h1>
+      <div className="flex gap-6 mt-8">
+        <Link
+          href={ROUTES.DASHBOARD}
+          className="w-52 border-solid border-2 border-indigo-500 rounded-md py-3 px-3 text-center"
+        >
+          Go to Dashboard
+        </Link>
+        <Link
+          href={ROUTES.PROFILE}
+          className="w-52 border-solid border-2 border-indigo-500 rounded-md py-3 px-3 text-center"
+        >
+          Go to Profile
+        </Link>
+      </div>
     </div>
   );
 }
