62774e309136cbb306619a5807971ccb6f21904f
Marek Miklaszewski <mmiklaszewski@apptension.com>
2023-04-26 13:14:42 +0200

    [RAB-4] Create transaction history

--- apps/next-app/src/pages/profile/index.tsx
+++ apps/next-app/src/pages/profile/index.tsx
@@ -5,12 +5,18 @@ import { GetServerSidePropsContext } from 'next';
 import { AvatarChanger } from 'shared/components/profile/AvatarChanger';
 import { EditProfileForm } from 'shared/components/profile/EditProfileForm';
 import { getApolloServerClient } from 'shared/services/apollo';
+import { EDGE_FUNCTION_NAMES } from 'constants/EDGE_FUNCTION_NAMES';
+import { Checkmark } from 'shared/components/Checkmark';
+import { Crossmark } from 'shared/components/Crossmark';
+import { getSubscriptionPrice } from 'utils/getSubscriptionPrice';
+import { format, fromUnixTime } from 'date-fns';
 
 interface ProfileProps {
   profile: GetProfileQuery['profilesCollection'];
+  charges: any;
 }
 
-const Profile = ({ profile }: ProfileProps) => {
+const Profile = ({ profile, charges }: ProfileProps) => {
   const userProfile = profile?.edges[0];
 
   return (
@@ -19,6 +25,30 @@ const Profile = ({ profile }: ProfileProps) => {
 
       <AvatarChanger profileAvatarSrc={userProfile?.node.avatar_url} />
       <EditProfileForm fullName={userProfile?.node.full_name!} />
+      <div className="max-w-xl w-full">
+        <h2 className="mt-8 text-2xl">Transaction history</h2>
+        <div className="flex flex-col">
+          {charges.charges.data.map(
+            ({ id, created, description, amount, currency, status }: any) => (
+              <div
+                key={id}
+                className="flex justify-between w-full border-b-2 border-slate-800 py-6"
+              >
+                <div>
+                  {description}{' '}
+                  <span className="text-slate-400">
+                    ({format(fromUnixTime(created), 'Pp')})
+                  </span>
+                </div>
+                <div>
+                  {getSubscriptionPrice(amount)} {currency.toUpperCase()}
+                </div>
+                {status === 'succeeded' ? <Checkmark /> : <Crossmark isError />}
+              </div>
+            )
+          )}
+        </div>
+      </div>
     </div>
   );
 };
@@ -34,5 +64,9 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
     query: GET_PROFILE,
     variables: { profileId: session.data.session?.user.id },
   });
-  return { props: { profile: data.profilesCollection } };
+
+  const { data: chargesData } = await supabase.functions.invoke(
+    EDGE_FUNCTION_NAMES.GET_STRIPE_CHARGES
+  );
+  return { props: { profile: data.profilesCollection, charges: chargesData } };
 };
--- apps/next-app/src/shared/components/Crossmark/crossmark.component.tsx
+++ apps/next-app/src/shared/components/Crossmark/crossmark.component.tsx
@@ -1,7 +1,15 @@
 import CrossmarkSVG from 'assets/cross-mark.svg';
 
-export const Crossmark = () => {
+interface CrossmarkProps {
+  isError?: boolean;
+}
+
+export const Crossmark = ({ isError }: CrossmarkProps) => {
   return (
-    <CrossmarkSVG className="w-4 h-4 mr-1.5 text-gray-400 flex-shrink-0" />
+    <CrossmarkSVG
+      className={`w-4 h-4 mr-1.5 ${
+        isError ? 'text-red-400' : 'text-gray-400'
+      } flex-shrink-0`}
+    />
   );
 };
