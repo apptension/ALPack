a14755350d803895b5959520cbc09880570d11a6
Michal Kleszcz <mkleszcz@apptension.com>
2023-09-07 14:20:53 +0200

    fix: Remove unused `SMTP_SECURE` env

--- packages/emails/src/config/transport.ts
+++ packages/emails/src/config/transport.ts
@@ -3,7 +3,6 @@ import SMTPTransport from 'nodemailer/lib/smtp-transport';
 export const transportConfig: SMTPTransport.Options = {
   host: process.env['SMTP_HOST'] ?? 'localhost',
   port: parseInt(process.env['SMTP_PORT'] || '', 10) || 587,
-  secure: !!process.env['SMTP_SECURE'] ?? false,
   auth: {
     user: process.env['SMTP_USERNAME'] ?? '',
     pass: process.env['SMTP_PASSWORD'] ?? '',
