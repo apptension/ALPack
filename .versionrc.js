505393de34875dea7b8252bd53037fca96d65ab9
Michał Kleszcz <mkleszcz@apptension.com>
2023-10-10 13:47:32 +0000

    feat: Add versioning setup

---
+++ .versionrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  bumpFiles: [
+    {
+      filename: './package.json',
+      type: 'json',
+    },
+    {
+      filename: './apps/app/package.json',
+      type: 'json',
+    },
+    {
+      filename: './packages/api-client/package.json',
+      type: 'json',
+    },
+    {
+      filename: './packages/core/package.json',
+      type: 'json',
+    },
+    {
+      filename: './packages/emails/package.json',
+      type: 'json',
+    },
+    {
+      filename: './packages/emails/package.json',
+      type: 'json',
+    },
+    {
+      filename: './packages/graphql-api/package.json',
+      type: 'json',
+    },
+    {
+      filename: './packages/internal/internal-core/package.json',
+      type: 'json',
+    },
+    {
+      filename: './packages/internal/internal-tools/package.json',
+      type: 'json',
+    },
+  ],
+  tagPrefix: '',
+};
